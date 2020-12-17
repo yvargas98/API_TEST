b09393eac6bdaf3c69e08e79e96a85dd5f22cd63
Yasmin Iveth Vargas Godoy <yasmin.vargas@laureate.net>
2020-12-17 15:14:06 -0600

    updated

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
-import express = require('express')
-import cors = require('cors')
-import router from '../src/routes/index'
-require('dotenv').config()
+import express from 'express';
+const app = express()
+const PORT : string|number = process.env.PORT || 5000;
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(router);
-
-app.listen(5000, () => {
-    console.log(`Server is running on port 5000`);
+app.use("*",(req, res) =>{
+    res.send("<h1>Welcome to your simple server! Awesome right</h1>");
 });
+
+app.listen(PORT,() => console.log(`hosting @${PORT}`));
