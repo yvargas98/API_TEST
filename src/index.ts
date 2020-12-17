823f8b9ea6bd19b7f7924268bb7de6a1674f4774
Yasmin Iveth Vargas Godoy <yasmin.vargas@laureate.net>
2020-12-17 16:43:18 -0600

    Heroku

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import express = require('express');
-const app = express();
-const PORT = 8080;
-
-app.use("*",(req: any, res: { send: (arg0: string) => void; }) =>{
-    res.send("<h1>Welcome!</h1>");
+import app from './App'
+const port = process.env.PORT || 3000;
+app.listen(port, (err) => {
+    if (err) {
+        console.error(`Error starting server: ${err}`);
+        process.abort();
+    }
+console.log(`Server is listening on port ${port}.`);
+    return;
 });
-
-app.listen(PORT,() => console.log(`Server is running on ${PORT}`));
--- src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-
-const router: Router = Router();
-
-router.get('/', async (req, res) => {
-    res.send('Welcome!');
-});
-
-export default router;
