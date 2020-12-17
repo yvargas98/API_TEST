f4e1886c5a2cc439c5d15d2aefcdc134b0ac80a3
Yasmin Iveth Vargas Godoy <yasmin.vargas@laureate.net>
2020-12-17 12:56:45 -0600

    change port

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,6 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(5000, () => {
+    console.log(`Server is running on port 5000`);
 });
