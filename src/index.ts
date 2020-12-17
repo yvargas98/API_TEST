eb3db55b4119e50b4415820944f947a3e81cb7a1
Yasmin Iveth Vargas Godoy <yasmin.vargas@laureate.net>
2020-12-17 15:19:14 -0600

    updated

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 const app = express()
 const PORT : string|number = process.env.PORT || 5000;
 
-app.use("*",(req, res) =>{
-    res.send("<h1>Welcome to your simple server! Awesome right</h1>");
+app.use("*",(req: Request, res: Response) =>{
+    res.send("<h1>Welcome!</h1>");
 });
 
-app.listen(PORT,() => console.log(`hosting @${PORT}`));
+app.listen(PORT,() => console.log(`Server is running on ${PORT}`));
