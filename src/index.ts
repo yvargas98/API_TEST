b5a4058bf1faa0a9b5ff0a8ac9e3aecf4f6c4a02
Yasmin Iveth Vargas Godoy <yasmin.vargas@laureate.net>
2020-12-17 15:25:50 -0600

    updated

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import express, { Request, Response } from 'express';
-const app = express()
+import express, {Request, Response } from "express";
+const app = express();
 const PORT : string|number = process.env.PORT || 5000;
 
 app.use("*",(req: Request, res: Response) =>{
